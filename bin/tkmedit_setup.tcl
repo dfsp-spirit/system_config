a038ea8827c1948583392395093d6a913f174ac6
Tim Schäfer <ts+code@rcmd.org>
2018-07-21 15:56:55 +0200

    add some tiny neuroimaging scripts in bin dir

---
+++ bin/tkmedit_setup.tcl
@@ -0,0 +1,7 @@
+# tkmedit_setup.tcl -- Tim's startup config file for manual brain edits in tkmedit.
+# This performs brush setup, so you do not have to do it for each image. To use this script, append '-tcl ~/path/to/tkmedit_setup.tcl' to your tkmedit command line.
+# See https://surfer.nmr.mgh.harvard.edu/fswiki/TkMeditGuide/TkMeditReference/TkMeditScripting for more commands.
+SetBrushShape 4 0 0
+SetBrushTarget 0
+SetSlice 200
+SetTool 2
