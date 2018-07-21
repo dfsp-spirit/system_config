346382368765d0ce5f34e3725227bdeb47082700
Tim Schäfer <ts+code@rcmd.org>
2018-07-21 15:57:25 +0200

    Add shell script to copy all files into place.

---
+++ install_sysconf.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# make sure this script is executed in the correct directory
+if [ ! -f bash_profile ]; then
+  echo 'ERROR: Execute this script from its directory.'
+  exit 1
+fi
+
+if [ ! -d ~/bin ]; then
+  mkdir ~/bin/
+fi
+cp -r bin/* ~/bin/
+
+cp ./bash_profile ~/.bash_profile
+cp ./bashrc ~/.bashrc
+
+# vim setup
+if [ ! -d ~/.vim/colors ]; then
+  mkdir -p  ~/.vim/colors
+fi
+cp vimrc ~/.vimrc
+cp tools/solarized/solarized.vim ~/.vim/colors/
+
+# ssh setup, shortcuts for hosts
+if [ ! -d ~/.ssh/ ]; then
+  mkdir -p  ~/.ssh
+fi
+cp ssh_config ~/.ssh/
